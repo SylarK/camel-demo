d016700be3544d10c40123702c926d7e3d4bccf0
Lucas Amado <syst.lucasamado@gmail.com>
2023-06-29 09:39:10 +0200

    Initial commit

---
+++ src/test/java/pt/amado/cameldemo/CamelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amado.cameldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
