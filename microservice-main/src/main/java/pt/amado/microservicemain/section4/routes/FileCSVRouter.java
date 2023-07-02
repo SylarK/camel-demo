8df7817ba7d0d40d488fe052f7f654358c0463ac
Lucas Amado <syst.lucasamado@gmail.com>
2023-07-02 14:57:13 +0200

    study: exploring dynamic routing eip

--- microservice-main/src/main/java/pt/amado/microservicemain/section4/routes/FileCSVRouter.java
+++ microservice-main/src/main/java/pt/amado/microservicemain/section4/routes/FileCSVRouter.java
@@ -1,13 +1,23 @@
 package pt.amado.microservicemain.section4.routes;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Body;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Header;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import pt.amado.microservicemain.section2.producer.model.CurrentExchange;
 import pt.amado.microservicemain.section4.util.ArrayListAggregationStrategy;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 @ConditionalOnProperty(
@@ -15,8 +25,11 @@ import java.util.List;
         havingValue = "4",
         matchIfMissing = true
 )
+@RequiredArgsConstructor
 public class FileCSVRouter extends RouteBuilder {
 
+    @Autowired
+    DynamicRouterBean dynamicRouterBean;
 
     @Override
     public void configure() throws Exception {
@@ -40,14 +53,37 @@ public class FileCSVRouter extends RouteBuilder {
 //                .to("activemq:split-route");
 
         //aggregate
-        from("file:files/aggregate-json")
-                .unmarshal().json(JsonLibrary.Jackson, CurrentExchange.class)
-                .aggregate(simple("${body.to}"), new ArrayListAggregationStrategy())
-                //.completionFromBatchConsumer()
-                .completionSize(3)
-                .convertBodyTo(String.class)
-                .log("Message sent")
-                .to("activemq:aggregate-json-route");
+//        from("file:files/aggregate-json")
+//                .unmarshal().json(JsonLibrary.Jackson, CurrentExchange.class)
+//                .aggregate(simple("${body.to}"), new ArrayListAggregationStrategy())
+//                //.completionFromBatchConsumer()
+//                .completionSize(3)
+//                .convertBodyTo(String.class)
+//                .log("Message sent")
+//                .to("activemq:aggregate-json-route");
+
+        //slip
+//        String paths = "direct:slip-endpoint-1,direct:slip-endpoint-2";
+//        from("timer:slip-pattern?period=8000")
+//                .transform().constant("Foo Bar")
+//                .routingSlip(simple(paths));
+//
+//        from("direct:slip-endpoint-1")
+//                .to("log:slip-endpoint-1");
+//
+//        from("direct:slip-endpoint-2")
+//                .to("log:slip-endpoint-2");
+
+        //dynamic routing
+        from("timer:dynamic-pattern?period=8000")
+                .transform().constant("Default messsage to dynamic pattern")
+                .dynamicRouter(method(dynamicRouterBean));
+
+        from("direct:dynamic-endpoint-1")
+                .to("log:dynamic-endpoint-1");
+
+        from("direct:dynamic-endpoint-2")
+                .to("log:dynamic-endpoint-2");
     }
 
     @Component
@@ -58,4 +94,31 @@ public class FileCSVRouter extends RouteBuilder {
         }
 
     }
+
+
+}
+
+@Component
+class DynamicRouterBean {
+
+    Logger log = LoggerFactory.getLogger(DynamicRouterBean.class);
+
+    int invocations;
+
+    public String decideTheNextEndpoint(@ExchangeProperties Map<String, String> properties,
+                                        @Headers Map<String, String> headers,
+                                        @Body String body) {
+
+        log.info("{} {} {}", properties, headers, body);
+        invocations++;
+
+        if(invocations == 1)
+            return "direct:dynamic-endpoint-1";
+        if(invocations == 2)
+            return "direct:dynamic-endpoint-2";
+
+        invocations = 0;
+        return null;
+    }
+
 }
