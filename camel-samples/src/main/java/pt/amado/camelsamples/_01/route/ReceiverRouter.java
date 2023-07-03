5ff115135ae0d8e9d02e42f2ebe935f61d97991d
Lucas Amado <syst.lucasamado@gmail.com>
2023-07-03 15:07:01 +0200

    sample: exploring route
    - the route split the input and process the information using a transformer
    - the aggregation strategy will return a list of users
    - the response model will be filled and sent to the target

---
+++ camel-samples/src/main/java/pt/amado/camelsamples/_01/route/ReceiverRouter.java
@@ -0,0 +1,47 @@
+package pt.amado.camelsamples._01.route;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+import pt.amado.camelsamples._01.model.User;
+import pt.amado.camelsamples._01.processor.UserTransformerCamel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ReceiverRouter extends RouteBuilder {
+
+    private final UserTransformerCamel userTransformer;
+
+    @Override
+    public void configure() throws Exception {
+
+        from("file:{{camel.filepath}}")
+                .routeId("Receive-Router-Main")
+                .split(xpath("//record"), new AggregationStrategyUsers())
+                    .streaming()
+                    .unmarshal().jacksonXml(User.class)
+                    .process(userTransformer::doSomething)
+                .end()
+                .bean(userTransformer , "generateResponse")
+                .to("log:logging-application")
+                .to("activemq:route-request-cc");
+
+
+    }
+
+    private static class AggregationStrategyUsers implements org.apache.camel.AggregationStrategy {
+        @Override
+        public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+            List<User> userList = oldExchange != null ? oldExchange.getIn().getBody(List.class) : new ArrayList<>();
+            User newUser = newExchange.getIn().getBody(User.class);
+            userList.add(newUser);
+
+            newExchange.getIn().setBody(userList);
+            return newExchange;
+        }
+    }
+}
